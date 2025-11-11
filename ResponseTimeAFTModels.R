1e53349cea7874ac76ddeb059bf105339d925487
asabrio <a.sabrio@wustl.edu>
2025-11-11 14:52:17 -0600

    Updated code files

--- ResponseTimeAFTModels.R
+++ ResponseTimeAFTModels.R
@@ -17,6 +17,7 @@ library(data.table)
 library(effects)
 library(car)
 library(cowplot)
+library(igraph)
 
 # -----------------------------
 # 2. File Paths
@@ -30,42 +31,61 @@ fig_dir <- "~/Desktop/Research"     # Folder to save plots
 my_data <- read_excel(file.path(data_dir, "Calls_for_Service_2018.xlsx"))
 
 # -----------------------------
-# 4. Data Cleaning
+# 4. Time Variables and Data Cleaning
 # -----------------------------
-# Keep only non-self-initiated calls and valid coordinates
-my_data <- my_data %>%
-  filter(SelfInitiated == "N" & Location != "(0.0, 0.0)")
+my_data <- subset(my_data, my_data$SelfInitiated == "N")
+my_data <- subset(my_data, my_data$Location != "(0.0, 0.0)")
 
+time_dispatch <- (my_data$TimeDispatch)
+time_arrive <- (my_data$TimeArrive)
+time_create <- (my_data$TimeCreate)
+time_close <- (my_data$TimeClosed)
+time = data.frame()
+time <- data.frame(
+  time_arrive = time_arrive,
+  time_create = time_create,
+  time_dispatch = time_dispatch,
+  time_close = time_close
+)
+
+time$time_create <- as.POSIXct(time$time_create, format = "%Y-%m-%dT%H:%M:%S")
+time$time_arrive <- as.POSIXct(time$time_arrive, format = "%Y-%m-%dT%H:%M:%S")
+time$time_dispatch <- as.POSIXct(time$time_dispatch, format = "%Y-%m-%dT%H:%M:%S")
+time$time_close <- as.POSIXct(time$time_close, format = "%Y-%m-%dT%H:%M:%S")
+
+my_data$ResponseTime <- as.numeric(difftime(time$time_arrive, time$time_create, 
+                                            units = "mins"))
+my_data$WaitTime <- as.numeric(difftime(time$time_dispatch, time$time_create, 
+                                        units = "mins"))
+my_data$HandlingTime <- as.numeric(difftime(time$time_arrive, time$time_dispatch, 
+                                            units = "mins"))
 # -----------------------------
-# 5. Time Variables
+# 5. Data Cleaning
 # -----------------------------
-# Convert character times to POSIXct
-my_data_NoZero <- my_data_NoZero %>%
-  mutate(
-    TimeCreate = as.POSIXct(TimeCreate, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeDispatch = as.POSIXct(TimeDispatch, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeArrive = as.POSIXct(TimeArrive, format = "%Y-%m-%dT%H:%M:%S")
-  ) %>%
-  mutate(
-    ResponseTime = as.numeric(difftime(TimeArrive, TimeCreate, units = "mins")),
-    DispatchTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    WaitTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    HandlingTime = as.numeric(difftime(TimeArrive, TimeDispatch, units = "mins"))
-  ) %>%
-  na.omit() %>%
-  filter(ResponseTime > 0)
+my_data <- subset(my_data, my_data$WaitTime >= 0)
+
+my_data <- na.omit(my_data)
+
+my_data <- subset(my_data, my_data$ResponseTime >= 0)
+
+my_data_NoZero <- subset(my_data, my_data$ResponseTime > 0)
+
+my_data_NoZero <- setDT(my_data_NoZero) # needs to be a data table to reformat time
+
+my_data_NoZero[, TimeCreate := as.POSIXct(TimeCreate, format="%Y-%m-%dT%H:%M:%S")]
+
+my_data_NoZero[, TimeDispatch := as.POSIXct(TimeDispatch, format="%Y-%m-%dT%H:%M:%S")]
 
 # -----------------------------
 # 6. Extract Latitude/Longitude
 # -----------------------------
-my_data_NoZero <- my_data_NoZero %>%
-  mutate(
-    Latitude = as.numeric(sub("\\((.*?),.*", "\\1", Location)),
-    Longitude = as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", Location))
-  ) %>%
-  filter(!is.na(Latitude) & !is.na(Longitude))
+my_data_NoZero$Latitude <- as.numeric(sub("\\((.*?),.*", "\\1", my_data_NoZero$Location))
 
-# Convert to sf object
+my_data_NoZero$Longitude <- as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", my_data_NoZero$Location))
+
+# Keep only rows with valid coordinates
+my_data_NoZero <- my_data_NoZero[!is.na(my_data_NoZero$Latitude) & !is.na(my_data_NoZero$Longitude), ]
+
 crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 4326)
 
 # -----------------------------
@@ -74,8 +94,11 @@ crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 
 no.B <- st_read(file.path(data_dir, "harvard_NOLA.shp")) %>%
   st_transform(crs = 4326)
 
-# Neighborhood adjacency matrix
-no.mat <- nb2mat(poly2nb(no.B), style = "B")
+no.B<-st_transform(no.B,crs=4326)
+no.B <- st_make_valid(no.B)
+no.mat<-nb2mat(poly2nb(no.B), style = "B")
+
+g0 <- graph_from_adjacency_matrix((no.mat+t(no.mat))/2, mode="undirected")
 
 # -----------------------------
 # 8. Assign Neighborhood Labels
@@ -104,16 +127,16 @@ crime_sf_final <- crime_sf_final %>%
     TRUE ~ InitialPriority
   ))
 
-
 # -----------------------------
 # 10. Extract Hour
 # -----------------------------
-crime_sf_final$Hour <- as.character(format(crime_sf_final$TimeCreate, "%H"))
+crime_sf_final$Hour <- 0
+crime_sf_final$Hour <- format(crime_sf_final$TimeCreate, "%H")
 crime_sf_final$Hour <- as.numeric(crime_sf_final$Hour)
 class(crime_sf_final$Hour) = "character"
 
-# add hour of day data for Handling Time
-crime_sf_final$HandlingHour <- as.character(format(crime_sf_final$TimeDispatch, "%H"))
+crime_sf_final$HandlingHour <- 0
+crime_sf_final$HandlingHour <- format(crime_sf_final$TimeDispatch, "%H")
 crime_sf_final$HandlingHour <- as.numeric(crime_sf_final$HandlingHour)
 class(crime_sf_final$HandlingHour) = "character"
 
@@ -182,45 +205,70 @@ Anova(RespTimeANOVA0)
 Anova(RespTimeANOVA00)
 Anova(RespTimeANOVA1)
 
+# -----------------------------
+# 14. Further Data Cleaning for Handling Time Model in step 5
+# Re-run Steps 4-11 for Handling Time Data
+# -----------------------------
 
 # -----------------------------
-# 14. Further Data Cleaning for Handling Time Model
+# 4. Time Variables and Data Cleaning
 # -----------------------------
+my_data <- subset(my_data, my_data$SelfInitiated == "N")
+my_data <- subset(my_data, my_data$Location != "(0.0, 0.0)")
+
+time_dispatch <- (my_data$TimeDispatch)
+time_arrive <- (my_data$TimeArrive)
+time_create <- (my_data$TimeCreate)
+time_close <- (my_data$TimeClosed)
+time = data.frame()
+time <- data.frame(
+  time_arrive = time_arrive,
+  time_create = time_create,
+  time_dispatch = time_dispatch,
+  time_close = time_close
+)
+
+time$time_create <- as.POSIXct(time$time_create, format = "%Y-%m-%dT%H:%M:%S")
+time$time_arrive <- as.POSIXct(time$time_arrive, format = "%Y-%m-%dT%H:%M:%S")
+time$time_dispatch <- as.POSIXct(time$time_dispatch, format = "%Y-%m-%dT%H:%M:%S")
+time$time_close <- as.POSIXct(time$time_close, format = "%Y-%m-%dT%H:%M:%S")
+
+my_data$ResponseTime <- as.numeric(difftime(time$time_arrive, time$time_create, 
+                                            units = "mins"))
+my_data$WaitTime <- as.numeric(difftime(time$time_dispatch, time$time_create, 
+                                        units = "mins"))
+my_data$HandlingTime <- as.numeric(difftime(time$time_arrive, time$time_dispatch, 
+                                            units = "mins"))
+# -----------------------------
+# 5. Data Cleaning
+# -----------------------------
+my_data <- subset(my_data, my_data$WaitTime >= 0)
+
+my_data <- na.omit(my_data)
+
+my_data <- subset(my_data, my_data$ResponseTime >= 0)
+
+my_data_NoZero <- subset(my_data, my_data$ResponseTime > 0)
+
+# additional cleaning here
 my_data_NoZero <- subset(my_data_NoZero, my_data_NoZero$HandlingTime > 0)
 
-# -----------------------------
-# 14.5 Re-run Steps 6-11 for Handling Time Data
-# -----------------------------
-# -----------------------------
-# 5. Time Variables
-# -----------------------------
-# Convert character times to POSIXct
-my_data_NoZero <- my_data_NoZero %>%
-  mutate(
-    TimeCreate = as.POSIXct(TimeCreate, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeDispatch = as.POSIXct(TimeDispatch, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeArrive = as.POSIXct(TimeArrive, format = "%Y-%m-%dT%H:%M:%S")
-  ) %>%
-  mutate(
-    ResponseTime = as.numeric(difftime(TimeArrive, TimeCreate, units = "mins")),
-    DispatchTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    WaitTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    HandlingTime = as.numeric(difftime(TimeArrive, TimeDispatch, units = "mins"))
-  ) %>%
-  na.omit() %>%
-  filter(ResponseTime > 0)
+my_data_NoZero <- setDT(my_data_NoZero) # needs to be a data table to reformat time
+
+my_data_NoZero[, TimeCreate := as.POSIXct(TimeCreate, format="%Y-%m-%dT%H:%M:%S")]
+
+my_data_NoZero[, TimeDispatch := as.POSIXct(TimeDispatch, format="%Y-%m-%dT%H:%M:%S")]
 
 # -----------------------------
 # 6. Extract Latitude/Longitude
 # -----------------------------
-my_data_NoZero <- my_data_NoZero %>%
-  mutate(
-    Latitude = as.numeric(sub("\\((.*?),.*", "\\1", Location)),
-    Longitude = as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", Location))
-  ) %>%
-  filter(!is.na(Latitude) & !is.na(Longitude))
+my_data_NoZero$Latitude <- as.numeric(sub("\\((.*?),.*", "\\1", my_data_NoZero$Location))
 
-# Convert to sf object
+my_data_NoZero$Longitude <- as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", my_data_NoZero$Location))
+
+# Keep only rows with valid coordinates
+my_data_NoZero <- my_data_NoZero[!is.na(my_data_NoZero$Latitude) & !is.na(my_data_NoZero$Longitude), ]
+
 crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 4326)
 
 # -----------------------------
@@ -229,8 +277,11 @@ crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 
 no.B <- st_read(file.path(data_dir, "harvard_NOLA.shp")) %>%
   st_transform(crs = 4326)
 
-# Neighborhood adjacency matrix
-no.mat <- nb2mat(poly2nb(no.B), style = "B")
+no.B<-st_transform(no.B,crs=4326)
+no.B <- st_make_valid(no.B)
+no.mat<-nb2mat(poly2nb(no.B), style = "B")
+
+g0 <- graph_from_adjacency_matrix((no.mat+t(no.mat))/2, mode="undirected")
 
 # -----------------------------
 # 8. Assign Neighborhood Labels
@@ -239,7 +290,6 @@ lst <- st_intersects(crime_sf$geometry, no.B$geometry)
 n <- nrow(crime_sf)
 crime_sf$nbhd <- as.numeric(lst[1:n])
 crime_sf$label <- no.B$gnocdc_lab[as.numeric(lst)]
-crime_sf <- subset(crime_sf, nbhd > 0)
 
 crime_sf_final <- subset(crime_sf, crime_sf$nbhd> 0)
 
@@ -263,14 +313,111 @@ crime_sf_final <- crime_sf_final %>%
 # -----------------------------
 # 10. Extract Hour
 # -----------------------------
-crime_sf_final$Hour <- as.character(format(crime_sf_final$TimeCreate, "%H"))
+crime_sf_final$Hour <- 0
+crime_sf_final$Hour <- format(crime_sf_final$TimeCreate, "%H")
 crime_sf_final$Hour <- as.numeric(crime_sf_final$Hour)
 class(crime_sf_final$Hour) = "character"
 
-# add hour of day data for Handling Time
-crime_sf_final$HandlingHour <- as.character(format(crime_sf_final$TimeDispatch, "%H"))
+crime_sf_final$HandlingHour <- 0
+crime_sf_final$HandlingHour <- format(crime_sf_final$TimeDispatch, "%H")
 crime_sf_final$HandlingHour <- as.numeric(crime_sf_final$HandlingHour)
 class(crime_sf_final$HandlingHour) = "character"
+# 
+# # -----------------------------
+# # 5. Time Variables
+# # -----------------------------
+# # Convert character times to POSIXct
+# my_data_NoZero <- my_data_NoZero %>%
+#   mutate(
+#     TimeCreate = as.POSIXct(TimeCreate, format = "%Y-%m-%dT%H:%M:%S"),
+#     TimeDispatch = as.POSIXct(TimeDispatch, format = "%Y-%m-%dT%H:%M:%S"),
+#     TimeArrive = as.POSIXct(TimeArrive, format = "%Y-%m-%dT%H:%M:%S")
+#   ) %>%
+#   mutate(
+#     ResponseTime = as.numeric(difftime(TimeArrive, TimeCreate, units = "mins")),
+#     DispatchTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
+#     WaitTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
+#     HandlingTime = as.numeric(difftime(TimeArrive, TimeDispatch, units = "mins"))
+#   ) %>%
+#   na.omit() %>%
+#   filter(ResponseTime > 0)
+# 
+# # -----------------------------
+# # 6. Extract Latitude/Longitude
+# # -----------------------------
+# my_data_NoZero <- my_data_NoZero %>%
+#   mutate(
+#     Latitude = as.numeric(sub("\\((.*?),.*", "\\1", Location)),
+#     Longitude = as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", Location))
+#   ) %>%
+#   filter(!is.na(Latitude) & !is.na(Longitude))
+# 
+# # Convert to sf object
+# crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 4326)
+# 
+# # -----------------------------
+# # 7. Load Neighborhood Shapefile
+# # -----------------------------
+# no.B <- st_read(file.path(data_dir, "harvard_NOLA.shp")) %>%
+#   st_transform(crs = 4326)
+# 
+# # Neighborhood adjacency matrix
+# no.mat <- nb2mat(poly2nb(no.B), style = "B")
+# 
+# # -----------------------------
+# # 8. Assign Neighborhood Labels
+# # -----------------------------
+# lst <- st_intersects(crime_sf$geometry, no.B$geometry)
+# n <- nrow(crime_sf)
+# crime_sf$nbhd <- as.numeric(lst[1:n])
+# crime_sf$label <- no.B$gnocdc_lab[as.numeric(lst)]
+# crime_sf <- subset(crime_sf, nbhd > 0)
+# 
+# crime_sf_final <- subset(crime_sf, crime_sf$nbhd> 0)
+# 
+# # -----------------------------
+# # 9. Recode Priority
+# # -----------------------------
+# # zeros <- c("0B", "0C", "0E", "0H", "0Z")
+# # ones <- c("1Z")
+# # twos <- c("2G", "2H", "2J", "2Q")
+# # threes <- c("3A", "3C")
+# # 
+# # crime_sf_final <- crime_sf_final %>%
+# #   mutate(InitialPriority = case_when(
+# #     InitialPriority %in% zeros ~ "0Other",
+# #     InitialPriority %in% ones  ~ "1C",
+# #     InitialPriority %in% twos  ~ "2Other",
+# #     InitialPriority %in% threes ~ "3",
+# #     TRUE ~ InitialPriority
+# #   ))
+# 
+# ones <- c("1B", "1S")
+# twos <- c("2", "2B", "2L")
+# threes <- c("3A", "3C")
+# for (i in 1:length(crime_sf_final$InitialPriority)){
+#   if (crime_sf_final$InitialPriority[i] %in% ones){
+#     crime_sf_final$InitialPriority[i] = "1J"
+#   }
+#   if (crime_sf_final$InitialPriority[i] %in% twos){
+#     crime_sf_final$InitialPriority[i] = "2A"
+#   }
+#   if (crime_sf_final$InitialPriority[i] %in% threes){
+#     crime_sf_final$InitialPriority[i] = "3"
+#   }
+# }
+# 
+# # -----------------------------
+# # 10. Extract Hour
+# # -----------------------------
+# crime_sf_final$Hour <- as.character(format(crime_sf_final$TimeCreate, "%H"))
+# crime_sf_final$Hour <- as.numeric(crime_sf_final$Hour)
+# class(crime_sf_final$Hour) = "character"
+# 
+# # add hour of day data for Handling Time
+# crime_sf_final$HandlingHour <- as.character(format(crime_sf_final$TimeDispatch, "%H"))
+# crime_sf_final$HandlingHour <- as.numeric(crime_sf_final$HandlingHour)
+# class(crime_sf_final$HandlingHour) = "character"
 
 # -----------------------------
 # 11. Prepare Variables for Analysis
--- ResponseTimeLogisticModel.R
+++ ResponseTimeLogisticModel.R
@@ -30,43 +30,63 @@ fig_dir <- "~/Desktop/Research"     # Folder to save plots
 my_data <- read_excel(file.path(data_dir, "Calls_for_Service_2018.xlsx"))
 
 # -----------------------------
-# 4. Data Cleaning
+# 4. Time Variables and Data Cleaning
 # -----------------------------
-# Keep only non-self-initiated calls and valid coordinates
-my_data <- my_data %>%
-  filter(SelfInitiated == "N" & Location != "(0.0, 0.0)")
+my_data <- subset(my_data, my_data$SelfInitiated == "N")
+my_data <- subset(my_data, my_data$Location != "(0.0, 0.0)")
 
+time_dispatch <- (my_data$TimeDispatch)
+time_arrive <- (my_data$TimeArrive)
+# time_arrive <- (my_data$TimeArrival)
+time_create <- (my_data$TimeCreate)
+time_close <- (my_data$TimeClosed)
+time = data.frame()
+time <- data.frame(
+  time_arrive = time_arrive,
+  time_create = time_create,
+  time_dispatch = time_dispatch,
+  time_close = time_close
+)
+
+time$time_create <- as.POSIXct(time$time_create, format = "%Y-%m-%dT%H:%M:%S")
+time$time_arrive <- as.POSIXct(time$time_arrive, format = "%Y-%m-%dT%H:%M:%S")
+time$time_dispatch <- as.POSIXct(time$time_dispatch, format = "%Y-%m-%dT%H:%M:%S")
+time$time_close <- as.POSIXct(time$time_close, format = "%Y-%m-%dT%H:%M:%S")
+
+my_data$ResponseTime <- as.numeric(difftime(time$time_arrive, time$time_create, 
+                                            units = "mins"))
+my_data$WaitTime <- as.numeric(difftime(time$time_dispatch, time$time_create, 
+                                        units = "mins"))
+my_data$HandlingTime <- as.numeric(difftime(time$time_arrive, time$time_dispatch, 
+                                            units = "mins"))
 # -----------------------------
-# 5. Time Variables
+# 5. Data Cleaning
 # -----------------------------
-# Convert character times to POSIXct
-my_data <- my_data %>%
-  mutate(
-    TimeCreate = as.POSIXct(TimeCreate, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeDispatch = as.POSIXct(TimeDispatch, format = "%Y-%m-%dT%H:%M:%S"),
-    TimeArrive = as.POSIXct(TimeArrive, format = "%Y-%m-%dT%H:%M:%S")
-  ) %>%
-  mutate(
-    ResponseTime = as.numeric(difftime(TimeArrive, TimeCreate, units = "mins")),
-    DispatchTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    WaitTime = as.numeric(difftime(TimeDispatch, TimeCreate, units = "mins")),
-    HandlingTime = as.numeric(difftime(TimeArrive, TimeDispatch, units = "mins"))
-  ) %>%
-  na.omit() %>%
-  filter(ResponseTime > 0)
+my_data <- subset(my_data, my_data$WaitTime >= 0)
+
+my_data <- na.omit(my_data)
+
+my_data <- subset(my_data, my_data$ResponseTime >= 0)
+
+my_data_NoZero <- subset(my_data, my_data$ResponseTime > 0)
+
+my_data_NoZero <- setDT(my_data_NoZero) # needs to be a data table to reformat time
+
+my_data_NoZero[, TimeCreate := as.POSIXct(TimeCreate, format="%Y-%m-%dT%H:%M:%S")]
+
+my_data_NoZero[, TimeDispatch := as.POSIXct(TimeDispatch, format="%Y-%m-%dT%H:%M:%S")]
 
 # -----------------------------
 # 6. Extract Latitude/Longitude
 # -----------------------------
-my_data <- my_data %>%
-  mutate(
-    Latitude = as.numeric(sub("\\((.*?),.*", "\\1", Location)),
-    Longitude = as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", Location))
-  ) %>%
-  filter(!is.na(Latitude) & !is.na(Longitude))
+my_data_NoZero$Latitude <- as.numeric(sub("\\((.*?),.*", "\\1", my_data_NoZero$Location))
 
-# Convert to sf object
-crime_sf <- st_as_sf(my_data, coords = c("Longitude", "Latitude"), crs = 4326)
+my_data_NoZero$Longitude <- as.numeric(sub(".*,\\s*(.*?)\\)", "\\1", my_data_NoZero$Location))
+
+# Keep only rows with valid coordinates
+my_data_NoZero <- my_data_NoZero[!is.na(my_data_NoZero$Latitude) & !is.na(my_data_NoZero$Longitude), ]
+
+crime_sf <- st_as_sf(my_data_NoZero, coords = c("Longitude", "Latitude"), crs = 4326)
 
 # -----------------------------
 # 7. Load Neighborhood Shapefile
@@ -74,8 +94,11 @@ crime_sf <- st_as_sf(my_data, coords = c("Longitude", "Latitude"), crs = 4326)
 no.B <- st_read(file.path(data_dir, "harvard_NOLA.shp")) %>%
   st_transform(crs = 4326)
 
-# Neighborhood adjacency matrix
-no.mat <- nb2mat(poly2nb(no.B), style = "B")
+no.B<-st_transform(no.B,crs=4326)
+no.B <- st_make_valid(no.B)
+no.mat<-nb2mat(poly2nb(no.B), style = "B")
+
+g0 <- graph_from_adjacency_matrix((no.mat+t(no.mat))/2, mode="undirected")
 
 # -----------------------------
 # 8. Assign Neighborhood Labels
@@ -84,7 +107,8 @@ lst <- st_intersects(crime_sf$geometry, no.B$geometry)
 n <- nrow(crime_sf)
 crime_sf$nbhd <- as.numeric(lst[1:n])
 crime_sf$label <- no.B$gnocdc_lab[as.numeric(lst)]
-crime_sf <- subset(crime_sf, nbhd > 0)
+
+crime_sf_final <- subset(crime_sf, crime_sf$nbhd> 0)
 
 # -----------------------------
 # 9. Recode Priority
@@ -106,16 +130,19 @@ crime_sf <- crime_sf %>%
 # -----------------------------
 # 10. Extract Hour
 # -----------------------------
-crime_sf$Hour <- as.character(format(crime_sf$TimeCreate, "%H"))
+crime_sf_final$Hour <- 0
+crime_sf_final$Hour <- format(crime_sf_final$TimeCreate, "%H")
+crime_sf_final$Hour <- as.numeric(crime_sf_final$Hour)
+class(crime_sf_final$Hour) = "character"
 
 # -----------------------------
 # 11. Prepare Variables for Analysis
 # -----------------------------
-yRT <- log(crime_sf$ResponseTime)
-yWT <- log(crime_sf$WaitTime)
-yHT <- log(crime_sf$HandlingTime)
+yRT <- log(crime_sf_final$ResponseTime)
+yWT <- log(crime_sf_final$WaitTime)
+yHT <- log(crime_sf_final$HandlingTime)
 
-dispatch <- as.numeric(crime_sf$WaitTime > 0)
+dispatch <- as.numeric(crime_sf_final$WaitTime > 0)
 dispatch0 <- which(dispatch == 0)
 dispatch1 <- which(dispatch == 1)
 
@@ -124,12 +151,12 @@ dispatch1 <- which(dispatch == 1)
 # -----------------------------
 z <- 1 - dispatch
 
-priority <- crime_sf$InitialPriority
-nbhd <- factor(crime_sf$nbhd)
-label <- factor(crime_sf$label)
+priority <- crime_sf_final$InitialPriority
+nbhd <- factor(crime_sf_final$nbhd)
+label <- factor(crime_sf_final$label)
 label <- relevel(label, ref = "LAKEVIEW")
-police <- factor(crime_sf$PoliceDistrict)
-hour <- factor(crime_sf$Hour)
+police <- factor(crime_sf_final$PoliceDistrict)
+hour <- factor(crime_sf_final$Hour)
 
 DispatchGLM <- glm(z ~ priority + police + label + hour, family = binomial)
 Anova(DispatchGLM)
